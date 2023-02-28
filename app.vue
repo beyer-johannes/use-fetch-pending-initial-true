299408d6ba4d09598f25c1e76e75b49a75ffa0d4
Johannes Beyer <info@beyer-johannes.de>
2023-02-28 12:37:38 +0100

    init

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup>
+const { pending, execute } = useFetch(
+  "https://api.chucknorris.io/jokes/random",
+  {
+    immediate: false,
+  }
+);
+</script>
+
+<template>
+  <div>
+    <div>Pending is: {{ pending }}</div>
+
+    <button @click="execute">Submit</button>
+  </div>
+</template>
